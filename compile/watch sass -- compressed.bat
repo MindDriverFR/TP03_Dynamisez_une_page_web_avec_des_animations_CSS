dcdb3381ecdf08ee0b2abdd0cf6ff0d2d0592f7f
nono <renaud.sannier@laposte.net>
2022-03-19 21:50:34 +0100

    Nettoyage code

--- compile/watch sass -- compressed.bat
+++ compile/watch sass -- compressed.bat
@@ -2,9 +2,6 @@ color 2
 
 cd C:\Users\Nono\Desktop\DEV\TP_openclassroom\TP03_Dynamisez_une_page_web_avec_des_animations_CSS\styles\sass
 cls
-sass style.scss ../style.css --watch --style compressed
+sass main.scss ../style.css --watch --style compressed
 
-cd C:\Users\Nono\Desktop\DEV\TP_openclassroom\TP03_Dynamisez_une_page_web_avec_des_animations_CSS\
-cls
-npm run prefix
 
