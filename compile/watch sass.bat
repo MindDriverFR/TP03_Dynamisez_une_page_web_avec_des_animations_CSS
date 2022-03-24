9ab980f5ff5d4e0a1227f5f7043c850a990478a7
nono <renaud.sannier@laposte.net>
2022-03-24 16:25:23 +0100

    changement nom dossier styles en css

--- compile/watch sass.bat
+++ compile/watch sass.bat
@@ -1,4 +1,4 @@
 color 2
-cd C:\Users\Nono\Desktop\DEV\TP_openclassroom\TP03_Dynamisez_une_page_web_avec_des_animations_CSS\styles\sass
+cd C:\Users\Nono\Desktop\DEV\TP_openclassroom\TP03_Dynamisez_une_page_web_avec_des_animations_CSS\css\sass
 cls
 sass main.scss ../style.css --watch
